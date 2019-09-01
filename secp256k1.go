ff981a03b6cc4abae74ba986334bfa9d92d53377
Eduardo Leegwater Simões <eduardols@protonmail.com>
2019-09-01 22:10:15 +0200

    Added ES256K-R and made tests better

--- secp256k1.go
+++ secp256k1.go
@@ -2,77 +2,143 @@
 package secp256k1
 
 import (
+	"crypto"
 	"crypto/ecdsa"
 	"errors"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/ethereum/go-ethereum/crypto"
+	ecrypto "github.com/ethereum/go-ethereum/crypto"
 )
 
-// SigningMethodES256K1 if the instance of the jwt.SigningMethod implementation.
-var SigningMethodES256K1 *SigningMethod
+// All the different instances of the algorithm. e.g. uport uses SigningMethodES256K1R
+var (
+	SigningMethodES256K1  *SigningMethodSecp256K1
+	SigningMethodES256K1R *SigningMethodSecp256K1
+)
 
-// SigningMethod is the implementation of jwt.SigningMethod.
-type SigningMethod struct {
-	alg string
+// SigningMethodSecp256K1 is the implementation of jwt.SigningMethod.
+type SigningMethodSecp256K1 struct {
+	alg      string
+	h        crypto.Hash
+	toRS     toRS
+	toOutSig toOutSig
 }
 
+// encodes a produced signature to the correct output - either in R || S or
+// R || S || V format.
+type toOutSig func(sig []byte) []byte
+
+// checks incoming signature and return it in R || S format.
+type toRS func(sig []byte) ([]byte, error)
+
 func init() {
-	SigningMethodES256K1 = &SigningMethod{
-		alg: "ES256K",
+	SigningMethodES256K1 = &SigningMethodSecp256K1{
+		alg:      "ES256K",
+		h:        crypto.SHA256,
+		toRS:     toRSES256K,
+		toOutSig: toES256K,
 	}
 	jwt.RegisterSigningMethod(SigningMethodES256K1.Alg(), func() jwt.SigningMethod {
 		return SigningMethodES256K1
 	})
+
+	SigningMethodES256K1R = &SigningMethodSecp256K1{
+		alg:      "ES256K-R",
+		h:        crypto.SHA256,
+		toRS:     toRSES256KR,
+		toOutSig: toES256KR,
+	}
+	jwt.RegisterSigningMethod(SigningMethodES256K1R.Alg(), func() jwt.SigningMethod {
+		return SigningMethodES256K1R
+	})
 }
 
-// Errors returned on different problems
+// Errors returned on different problems.
 var (
-	ErrWrongKeyFormat = errors.New("wrong key format")
-	ErrVerification   = errors.New("signature verification failed")
-	ErrFailedSigning  = errors.New("failed generating signature")
+	ErrWrongKeyFormat  = errors.New("wrong key format")
+	ErrVerification    = errors.New("signature verification failed")
+	ErrFailedSigning   = errors.New("failed generating signature")
+	ErrHashUnavailable = errors.New("hasher unavailable")
 )
 
 // Verify verifies a secp256k1 signature given an *ecdsa.PublicKey.
-func (sm *SigningMethod) Verify(signingString, signature string, key interface{}) error {
+func (sm *SigningMethodSecp256K1) Verify(signingString, signature string, key interface{}) error {
 	pubKey, ok := key.(*ecdsa.PublicKey)
 	if !ok {
 		return ErrWrongKeyFormat
 	}
-	pub := crypto.FromECDSAPub(pubKey)
+	pub := ecrypto.FromECDSAPub(pubKey)
+
+	if !sm.h.Available() {
+		return ErrHashUnavailable
+	}
+	hasher := sm.h.New()
+	hasher.Write([]byte(signingString))
 
 	sig, err := jwt.DecodeSegment(signature)
 	if err != nil {
 		return err
 	}
 
-	hash := crypto.Keccak256([]byte(signingString))
+	rs, err := sm.toRS(sig)
+	if err != nil {
+		return ErrVerification
+	}
 
-	if !crypto.VerifySignature(pub, hash, sig[:64]) {
+	if !ecrypto.VerifySignature(pub, hasher.Sum(nil), rs) {
 		return ErrVerification
 	}
 
 	return nil
 }
 
-// Sign generates a secp256k1 signature given an *ecdsa.PublicKey.
-func (sm *SigningMethod) Sign(signingString string, key interface{}) (string, error) {
+// Sign generates a secp256k1 signature given an *ecdsa.PrivateKey.
+func (sm *SigningMethodSecp256K1) Sign(signingString string, key interface{}) (string, error) {
 	prv, ok := key.(*ecdsa.PrivateKey)
 	if !ok {
 		return "", ErrWrongKeyFormat
 	}
 
-	hash := crypto.Keccak256([]byte(signingString))
+	if !sm.h.Available() {
+		return "", ErrHashUnavailable
+	}
+	hasher := sm.h.New()
+	hasher.Write([]byte(signingString))
 
-	sig, err := crypto.Sign(hash, prv)
+	sig, err := ecrypto.Sign(hasher.Sum(nil), prv)
 	if err != nil {
 		return "", ErrFailedSigning
 	}
+	out := sm.toOutSig(sig)
 
-	return jwt.EncodeSegment(sig), nil
+	return jwt.EncodeSegment(out), nil
 }
 
 // Alg returns the algorithm name
-func (sm *SigningMethod) Alg() string {
+func (sm *SigningMethodSecp256K1) Alg() string {
 	return sm.alg
+}
+
+func toES256K(sig []byte) []byte {
+	return sig[:64]
+}
+
+func toES256KR(sig []byte) []byte {
+	return sig
+}
+
+func toRSES256K(sig []byte) ([]byte, error) {
+	if len(sig) != 64 {
+		return nil, ErrVerification
+	}
+
+	return sig, nil
+}
+
+func toRSES256KR(sig []byte) ([]byte, error) {
+	if len(sig) != 65 {
+		return nil, ErrVerification
+	}
+
+	return sig[:64], nil
 }
--- secp256k1_test.go
+++ secp256k1_test.go
@@ -3,6 +3,7 @@ package secp256k1_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	secp256k1 "github.com/ureeves/jwt-go-secp256k1"
 
@@ -10,39 +11,62 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func TestSigningMethod(t *testing.T) {
+const TESTKEY = "f503f5eb94ff210c2d155e9d1c72545fe08f30baf5cd3dfbc1eb623d7bf946c6"
 
-	crypto.GenerateKey()
+func TestVerification(t *testing.T) {
 
-	key, err := crypto.GenerateKey()
+	key, err := crypto.HexToECDSA(TESTKEY)
 	if err != nil {
-		t.Fatalf("failed generating key: %s", err)
+		t.Fatalf("failed parsing key: %s", err)
 	}
 
-	// Create the Claims
-	claims := &jwt.StandardClaims{
-		Issuer: "test",
-	}
-
-	expectedToken := jwt.NewWithClaims(secp256k1.SigningMethodES256K1, claims)
-
-	tokenString, err := expectedToken.SignedString(key)
-	if err != nil {
-		t.Fatalf("failed getting signedString: %s", err)
-	}
-
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*secp256k1.SigningMethod); !ok {
+	_, err = jwt.Parse("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NjczNjgxMzEsImV4cCI6MzMxMjQ5NjgxMzEsInBlcm1pc3Npb25zIjpbIm5vdGlmaWNhdGlvbnMiXSwiY2FsbGJhY2siOiIvY2FsbGJhY2siLCJ0eXBlIjoic2hhcmVSZXEiLCJpc3MiOiJkaWQ6ZXRocjoweDEzZWQ4OGQ3NDRmQ2QwMGE1RDJlQTlhYzUwQjMwMjdGMkE1NENDYzIifQ.yPT8Q2YeWNnwiAcRkFzwKo53FWREYeW4HGuPw4gVBJlY61iRetokOXJMNFrDsP5rGGutaWn3-Z3QwRRvHLeu6wA", func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*secp256k1.SigningMethodSecp256K1); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 
 		return &key.PublicKey, nil
 	})
+
 	if err != nil {
-		t.Fatalf("failed parsing: %s", err)
+		t.Errorf("failed parsing: %s", err)
 	}
 
-	if token.Raw != tokenString {
-		t.Fatalf("parse->unparse not equal tokens")
+	_, err = jwt.Parse("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpYXQiOjE1NjczNjgxMzEsImF1ZCI6ImRpZDp1cG9ydDoyb3NuZko0V3k3TEJBbTJuUEJYaXJlMVdmUW43NVJyVjZUcyIsImlzcyI6ImRpZDp1cG9ydDoyb3NuZko0V3k3TEJBbTJuUEJYaXJlMVdmUW43NVJyVjZUcyJ9.Rlx4R2UOhg7UupIbZ-yf1bnf7jesTLXVq6jvEJC2LixEHl3gpgyYDGon_id-HPcg6wXF9dFtf8c1cbNPkeuaWQ", func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*secp256k1.SigningMethodSecp256K1); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return &key.PublicKey, nil
+	})
+
+	if err != nil {
+		t.Errorf("failed parsing: %s", err)
+	}
+}
+
+func TestGeneration(t *testing.T) {
+
+	key, err := crypto.HexToECDSA(TESTKEY)
+	if err != nil {
+		t.Fatalf("failed parsing key: %s", err)
+	}
+
+	token := jwt.NewWithClaims(secp256k1.SigningMethodES256K1, jwt.StandardClaims{
+		IssuedAt: time.Now().Unix(),
+	})
+
+	_, err = token.SignedString(key)
+	if err != nil {
+		t.Fatalf("failed signing token: %s", err)
+	}
+
+	token = jwt.NewWithClaims(secp256k1.SigningMethodES256K1R, jwt.StandardClaims{
+		IssuedAt: time.Now().Unix(),
+	})
+
+	_, err = token.SignedString(key)
+	if err != nil {
+		t.Fatalf("failed signing token: %s", err)
 	}
 }
